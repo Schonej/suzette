d25bb0b2781bc66b94645b6104bfcefea7741ee7
Justin <leftynation@gmail.com>
2025-03-11 14:45:35 -0600

    fixes

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export", // Enables static export
   basePath: "/suzette", // Replace with your GitHub repo name
-  assetPrefix: "/suzette", // Ensures assets load correctly
+  assetPrefix: "/suzette/", // Ensures assets load correctly
   images: {
     unoptimized: true, // Disables image optimization (not supported in static exports)
   },
